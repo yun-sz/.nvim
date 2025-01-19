477e5c48eeb38821b8921733f4a14681fe0830da
david4i <notdavidzin@gmail.com>
2025-01-19 00:45:14 -0300

    feat: change fterm keymap

--- lua/yun/plugins/fterm.lua
+++ lua/yun/plugins/fterm.lua
@@ -11,7 +11,8 @@ return {
             }
         })
         vim.api.nvim_set_keymap('n', '<leader>t', '<CMD>lua require("FTerm").toggle()<CR>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('t', '<leader>t', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', { noremap = true, silent = true })
+        vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Enter Normal Mode in Terminal" })
+        -- vim.api.nvim_set_keymap('t', '<leader>t', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', { noremap = true, silent = true })
     end
 }
 
