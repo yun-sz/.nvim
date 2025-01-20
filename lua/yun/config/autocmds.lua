cb73c3aa97997d7667431ed19b6a45f3c5533042
david4i <notdavidzin@gmail.com>
2025-01-19 22:52:09 -0300

    feat: add prettier alternative when saving js files

--- lua/yun/config/autocmds.lua
+++ lua/yun/config/autocmds.lua
@@ -1,8 +1,12 @@
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.vue" },
   callback = function()
-    vim.cmd("EslintFixAll")
-  end,
+        if vim.fn.exists(":EslintFixAll") == 2 then
+            vim.cmd("EslintFixAll")
+        elseif vim.fn.executable("prettier") == 1 then
+            vim.cmd(string.format("silent !prettier --write %s", vim.fn.shellescape(vim.fn.expand("%:p"))))
+        end
+    end,
 })
 
 
